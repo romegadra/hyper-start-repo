c30b709d4e3dba1443de35ad2204930ca73a1386
romegadra <rodrigo.medina.ramg@gmail.com>
2018-01-22 11:50:47 -0600

    Adding project structure

---
+++ JavaCoreTraining/src/hyperion/com/training/basic/TrainingBasic.java
@@ -0,0 +1,12 @@
+package hyperion.com.training.basic;
+
+/**
+ * Created by ro on 1/22/2018.
+ */
+public class TrainingBasic {
+
+    public static void main(String...args) {
+        System.out.println("Start here!!");
+    }
+
+}
