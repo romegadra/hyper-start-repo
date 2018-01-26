fdaa0852a9851b7414a0bdc1098e166f39e5ae8d
Hiram Padilla <hirampadillautd@gmail.com>
2018-01-25 18:09:28 -0600

    aqui voy

---
+++ JavaBasic/src/main/java/com/hyperion/training/basic/HiramClass.java
@@ -0,0 +1,13 @@
+package com.hyperion.training.basic;
+/* created by Hiram Padilla*/
+public class HiramClass 
+{
+
+	public void mensaje()
+	{System.out.println("Esta es mi clase en JAVA");}
+	
+	public static void main (String [] args)
+	{
+		
+	}
+}
