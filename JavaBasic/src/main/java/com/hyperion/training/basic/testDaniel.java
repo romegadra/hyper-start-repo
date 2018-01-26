6b92551600a058c46958b554fec3f9822e8234b8
DanielNevarez <daaniel31@gmail.com>
2018-01-25 18:23:22 -0600

    Te mamas Diego

--- JavaBasic/src/main/java/com/hyperion/training/basic/testDaniel.java
+++ JavaBasic/src/main/java/com/hyperion/training/basic/testDaniel.java
@@ -2,9 +2,15 @@ package com.hyperion.training.basic;
 
 public class testDaniel
 {
+	
 	public static void main(String[] args) 
 	{
+		new testDaniel().nameGit();
+			
+	}
 
-		
+	public String nameGit(String name)
+	{		
+		return name;		
 	}
 }
