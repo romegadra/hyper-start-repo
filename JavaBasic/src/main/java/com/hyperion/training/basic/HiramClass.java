c323bc2a4c328bbd34d0d3f4d938f8d510d8ce80
Hiram Padilla <hirampadillautd@gmail.com>
2018-01-25 18:26:22 -0600

    msj

--- JavaBasic/src/main/java/com/hyperion/training/basic/HiramClass.java
+++ JavaBasic/src/main/java/com/hyperion/training/basic/HiramClass.java
@@ -6,8 +6,14 @@ public class HiramClass
 	public void mensaje()
 	{System.out.println("Esta es mi clase en JAVA");}
 	
+	public void texto()
+	{
+		String variable = "Esto esta geniaaaal 8) ";
+	}
+	
 	public static void main (String [] args)
 	{
+		
 		
 	}
 }
