b9bd6cd04eeece92d36a54f25ea443a849e1696c
Hiram Padilla <hirampadillautd@gmail.com>
2018-01-27 13:14:22 -0600

    cambios en la clase hiram27ene

--- JavaBasic/src/main/java/com/hyperion/training/basic/Hiram_27_ene.java
+++ JavaBasic/src/main/java/com/hyperion/training/basic/Hiram_27_ene.java
@@ -1,8 +1,23 @@
 package com.hyperion.training.basic;
 
-public class Hiram_27_ene {
-
-	public static void main(String[] args) {
+public class Hiram_27_ene 
+{
+	/* creado por Hiram Padilla */
+	
+	public void sumar()
+	{}
+	
+	public void restar()
+	{}
+	
+	public void multiplicar()
+	{}
+	
+	public void dividir()
+	{}
+	
+	public static void main(String[] args) 
+	{
 		// TODO Auto-generated method stub
 
 	}
